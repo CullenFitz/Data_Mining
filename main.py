f8268c2e78a14420908238ff28fbf10d9bcb4e0d
cullenfitzgerald <cullenfitzgerald9@gmail.com>
2022-02-01 15:00:14 -0500

    finding eigens

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 #Question 3:
 
+import numpy as np
+
+
+#Factorize Sigma into its eigenvalues and eigenvectors
+sig = np.array([[1,1,1],
+                [1,3,3],
+                [1,3,6]])
+
+w, v = np.linalg.eig(sig)
+
+print(w)    #These are the eigenvalues
+print(v)    #The columns in this are eigenvectors corresponding to the eigenvalues
+
+
+
 
 
