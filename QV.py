9788312ca02d4adf8f881d0fd15677dc84c0b308
cullenfitzgerald <cullenfitzgerald9@gmail.com>
2022-02-04 17:39:20 -0500

    start q5

---
+++ QV.py
@@ -0,0 +1,6 @@
+# Estimate probabilities of each word
+
+words = ["charge", "glass", "faith", "then", "alone", "passion",
+         "corner", "must", "somewhat", "dropped", "one",
+         "seemed", "give", "flower"]
+
