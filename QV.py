12e6aa5884b7918075adcbaea7b10ba7fbaa4303
Cullen <cullenfitzgerald9@gmail.com>
2022-02-04 18:41:24 -0500

    iterating through one file

--- QV.py
+++ QV.py
@@ -1,6 +1,24 @@
-# Estimate probabilities of each word
+import pandas as pd
+import numpy as np
+import nltk
+import os
 
 words = ["charge", "glass", "faith", "then", "alone", "passion",
          "corner", "must", "somewhat", "dropped", "one",
          "seemed", "give", "flower"]
+
+with open(r'C:\Users\Cullen\Downloads\AABooks\Charles Dickens - cleared/BARNABY RUDGE.txt', encoding="utf8") as file:
+    long_description = file.read()
+    split = long_description.split()
+    wordCount = len(split)
+    print(wordCount)
+
+prob = []
+for i in words:
+    occ = long_description.count(i)
+    prob.append(occ/wordCount)
+for j in prob:
+    print(j)
+
+
 
