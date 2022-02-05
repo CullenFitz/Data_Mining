7d8ac0e3a0a017cecf388b948837451d08686952
Cullen <cullenfitzgerald9@gmail.com>
2022-02-04 19:49:02 -0500

    iterating through entire folder

--- QV.py
+++ QV.py
@@ -7,18 +7,31 @@ words = ["charge", "glass", "faith", "then", "alone", "passion",
          "corner", "must", "somewhat", "dropped", "one",
          "seemed", "give", "flower"]
 
-finArr = [["charge", "glass", "faith", "then", "alone", "passion",
-         "corner", "must", "somewhat", "dropped", "one",
-         "seemed", "give", "flower"]]
-with open(r'C:\Users\Cullen\Downloads\AABooks\Charles Dickens - cleared/BARNABY RUDGE.txt', encoding="utf8") as file:
-    long_description = file.read()
-    split = long_description.split()
-    wordCount = len(split)
-
+finArr = []
 prob = []
-for i in words:
-    occ = long_description.count(i)
-    prob.append(occ/wordCount)
-    finArr.append(prob)
-for i in finArr:
-    print(i)
+sources = []
+
+dickens = r"C:\Users\Cullen\Downloads\AABooks\Charles Dickens - cleared"
+eliot = r"C:\Users\Cullen\Downloads\AABooks\George Eliot - cleared"
+hardy = r"C:\Users\Cullen\Downloads\AABooks\Thomas Hardy - cleared"
+
+sources.append(dickens)
+sources.append(eliot)
+sources.append(hardy)
+
+for source in sources:
+    for filename in os.listdir(source):
+        f = os.path.join(source, filename)
+        if os.path.isfile(f):
+            with open(f,
+                      encoding="utf8") as file:
+                long_description = file.read()
+                split = long_description.split()
+                wordCount = len(split)
+
+                for i in words:
+                    occ = long_description.count(i)
+                    prob.append(occ / wordCount)
+                finArr.append(prob)
+
+print(finArr)
