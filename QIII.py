c70b1f17a81a7721b722a4e5a92b9c09d9e14994
Cullen <cullenfitzgerald9@gmail.com>
2022-02-10 20:03:01 -0500

    Finding contours

--- QIII.py
+++ QIII.py
@@ -1,25 +1,33 @@
-#Question 3:
+from numpy import random
 import numpy as np
-import scipy.stats as sci
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
-
-#Factorize Sigma into its eigenvalues and eigenvectors
-sig = np.array([[1,1,1],
-                [1,3,3],
-                [1,3,6]])
-
-w, v = np.linalg.eig(sig)
-
-#print(w)    #These are the eigenvalues
-#print(v)    #The columns in this are eigenvectors corresponding to the eigenvalues
-
-rv = sci.multivariate_normal(mean=None, cov=sig, seed=1)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
 
 
 
+mu = [0,0,0]
+mu = np.transpose(mu)
 
+cov = [[1,1,1], [1,3,3], [1,3,6]]
 
+v, w = np.linalg.eig(cov)
 
+dist = random.multivariate_normal(mu, cov, size=1000)
 
+xdata = []
+ydata = []
+zdata = []
+for i in dist:
+    xdata.append(i[0])
+    ydata.append(i[1])
+    zdata.append(i[2])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens')
 
+plt.show()
 
